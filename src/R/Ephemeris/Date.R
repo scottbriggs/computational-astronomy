9af3d9533dd11b9e0d0614ddc3408a3af471882f
Scott Briggs <hopingforclearskies@gmail.com>
2025-05-02 22:43:51 -0500

    Added function to calculate the date of Easter

--- src/R/Ephemeris/Date.R
+++ src/R/Ephemeris/Date.R
@@ -119,3 +119,55 @@ DayOfWeek <- function(jdn)
   
   return (dow)
 }
+
+# Caludate the date of Easter in the Gregorian and Julian calendars
+DateOfEaster <- function(year)
+{
+  month <- ""
+  day <- 0
+  
+  # Gregorian Calendar
+  if (year >= 1583) {
+    a <- year %% 19
+    b <- year %/% 100
+    c <- year %% 100
+    d <- b %/% 4
+    e <- b %% 4
+    f <- (b + 8) %/% 25
+    g <- (b - f + 1) %/% 3
+    h <- (19 * a + b - d - g + 15) %% 30
+    i <- c %/% 4
+    k <- c %% 4
+    l <- (32 + 2 * e + 2 * i - h - k) %% 7
+    m <- (a + 11 * h + 22 * l) %/% 451
+    n <- (h + l - 7 * m + 114) %/% 31
+    p <- (h + l - 7 * m + 114) %% 31
+    
+    if (n == 3){
+      month <- "March"
+    } else if (n == 4){
+      month <- "April"
+    }
+    
+    day <- p + 1
+    
+  } else { # Julian calendar
+    a <- year %% 4
+    b <- year %% 7
+    c <- year %% 19
+    d <- (19 * c + 15) %% 30
+    e <- (2 * a + 4 * b - d + 34) %% 7
+    f <- (d + e + 114) %/% 31
+    g <- (d + e + 114) %% 31
+    
+    if (f == 3){
+      month <- "March"
+    } else if (f == 4){
+      month <- "April"
+    }
+    
+    day <- g + 1
+  }
+  
+  return(c(month, day))
+}
